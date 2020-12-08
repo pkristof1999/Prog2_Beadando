89a0e41063c1bbf32ee43d40337fde41985336b3
Kristóf Pankotai <p.kristof1999@gmail.com>
2020-12-08 23:01:39 +0100

    Hibakezelés hozzáadva

--- beadando_calc.py
+++ beadando_calc.py
@@ -1,12 +1,51 @@
-#NOT FINISHED
-
 class Calculate:
     def calculateValues(self, a):
-        sumOfValues = int(a.getKliszt()) + int(a.getViz()) + int(a.getEleszto()) + int(a.getSo()) + int(a.getFliszt())
-        timeToProduce = int(a.getIdo())
-        allIncome = sumOfValues * timeToProduce
-        profit = int(allIncome) - sumOfValues
-        showTxt = f"Darabszám: {int(a.getDb())} Db\nBeruházás: {sumOfValues:,} Ft\nIdő: {timeToProduce} Óra\nBevétel: {allIncome:,} Ft\nProfit: {profit:,} Ft".replace(",", " ")
+        kliszt = a.getKliszt()
+        viz = a.getViz()
+        eleszto = a.getEleszto()
+        so = a.getSo()
+        fliszt = a.getFliszt()
+        db = a.getDb()
+        ido = a.getIdo()
+        
+        try:
+            if kliszt == "0" or \
+               viz == "0" or \
+               eleszto == "0" or \
+               so == "0" or \
+               fliszt == "0" or \
+               db == "0":
+                    raise Exception
+            
+            if kliszt == ".0":
+                    kliszt = "0.0"
+                    kliszt = float(kliszt)
+            if viz == ".0":
+                    viz = "0.0"
+                    viz = float(viz)
+            if eleszto == ".0":
+                    eleszto = "0.0"
+                    eleszto = float(eleszto)
+            if so == ".0":
+                    so = "0.0"
+                    so = float(so)
+            if fliszt == ".0":
+                    fliszt = "0.0"
+                    fliszt = float(fliszt)
+            if db == ".0":
+                    db == "0.0"
+                    db = float(db)
+        
+            sumOfValues = (float(kliszt) + float(viz) + float(eleszto) + float(so) + float(fliszt)) * float(db)
+            allIncome = sumOfValues * ido
+            profit = float(allIncome) - sumOfValues
+            showTxt = f"Darabszám: {int(db)} Db\nBeruházás: {sumOfValues:,} Ft\nIdő: {ido} Óra\nBevétel: {allIncome:,} Ft\nProfit: {profit:,} Ft".replace(",", " ")
+
+        except ValueError:
+            return "Hiba! Egy vagy több értéket kihagyott / rosszul adta meg!"
+        except Exception:
+            return "Hiba! Egy vagy több értéknél 0 a megadott érték!\nHa mégis 0-val számolna, így adja meg: .0"
+
         return showTxt
 
     def storeValues(self, a, v_kliszt, v_viz, v_eleszto, v_so, v_fliszt, v_db, v_ido):
