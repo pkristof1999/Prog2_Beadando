495b05a2b2ac1968f491113983f1dbe28aec6177
Kristóf Pankotai <p.kristof1999@gmail.com>
2020-12-09 19:59:57 +0100

    Frissítve

--- beadando.py
+++ beadando.py
@@ -159,7 +159,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Rozskenyér Árkalkulátor"))
-        #MainWindow.setWindowIcon(QtGui.QIcon(QtGui.QPixmap("<Elérési útvonal>")))
         self.termek_neve.setText(_translate("MainWindow", "Rozskenyér"))
         self.alapanyag_1.setText(_translate("MainWindow", "Kenyérliszt beszerzési ára (Ft/db):"))
         self.alapanyag_2.setText(_translate("MainWindow", "Víz beszerzési ára (Ft/db):"))
